8cc390117e65acf1d30552a1835fc8c5b2faae8c
Katsujukou <katsujukou@gmail.com>
2023-01-23 22:54:36 +0900

    Simplify Store

--- src/Halogen/Helix/Store.purs
+++ src/Halogen/Helix/Store.purs
@@ -18,54 +18,39 @@ import Effect.Unsafe (unsafePerformEffect)
 import Halogen.Subscription as HS
 
 newtype HelixStore s a = HelixStore
-  { state :: HelixState s
-  , stepper :: HelixStepper s a
-  }
-
-type HelixState s =
-  { value :: Ref s
+  { state :: Ref s
+  , dispatch :: a -> Effect Unit
   , emitter :: HS.Emitter s
   , listener :: HS.Listener s
   }
 
-type HelixStepper s a =
-  { action :: HS.SubscribeIO a
-  , state :: HS.SubscribeIO s
-  }
-
 getState :: forall m s a. MonadEffect m => HelixStore s a -> m s
-getState (HelixStore { state }) = liftEffect $ Ref.read state.value
+getState (HelixStore { state }) = liftEffect $ Ref.read state
 
 dispatch :: forall m s a. MonadEffect m => HelixStore s a -> a -> m Unit
-dispatch (HelixStore { stepper: { action: { listener } } }) action = liftEffect do
-  HS.notify listener action
+dispatch (HelixStore s) action = liftEffect $ s.dispatch action
 
 emitState :: forall s a. HelixStore s a -> HS.Emitter s
-emitState (HelixStore { state }) = state.emitter
+emitState (HelixStore { emitter }) = emitter
 
 mkHelixStore :: forall s a. String -> s -> (s -> a -> s) -> Effect (HelixStore s a)
 mkHelixStore id initial reducer = runEffectFn2 unsafeGetOrCache id $
   defer \_ -> unsafePerformEffect $ do
-    stepper <- do
-      action <- HS.create
-      state <- HS.create
-      pure { action, state }
-    state <- do
-      value <- Ref.new initial
-      { emitter, listener } <- HS.create
-      pure { value, emitter, listener }
-    setupFlow reducer state stepper
-    pure $ HelixStore { state, stepper }
+    state <- Ref.new initial
+    { emitter, listener } <- HS.create
 
-setupFlow :: forall s a. (s -> a -> s) -> HelixState s -> HelixStepper s a -> Effect Unit
-setupFlow reducer st stepper = do
-  void $ HS.subscribe stepper.action.emitter \action -> do
-    current <- Ref.read st.value
-    let newState = reducer current action
-    HS.notify stepper.state.listener newState
+    let
+      dispatchImpl action = do
+        currentState <- Ref.read state
+        let newState = reducer currentState action
+        Ref.write newState state
+        HS.notify listener newState
 
-  void $ HS.subscribe stepper.state.emitter \state -> do
-    Ref.write state st.value
-    HS.notify st.listener state
+    pure $ HelixStore
+      { state
+      , dispatch: dispatchImpl
+      , listener
+      , emitter
+      }
 
 foreign import unsafeGetOrCache :: forall a. EffectFn2 String (Lazy a) a
