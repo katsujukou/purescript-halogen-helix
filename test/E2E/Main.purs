db62015078aa98fd9dd835a00e47a935d59c6db6
Katsujukou Kineya <katsujukou@gmail.com>
2024-09-05 10:39:15 +0900

    Use spec-node

--- test/E2E/Main.purs
+++ test/E2E/Main.purs
@@ -3,10 +3,9 @@ module Test.E2E.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff (launchAff_)
 import Test.E2E.Spec (spec)
 import Test.Spec.Reporter (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] spec
+main = runSpecAndExitProcess [ consoleReporter ] spec
--- test/Unit/Main.purs
+++ test/Unit/Main.purs
@@ -3,10 +3,9 @@ module Test.Unit.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff (launchAff_)
 import Test.Spec.Reporter (consoleReporter)
-import Test.Spec.Runner (runSpec)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
 import Test.Unit.Spec (spec)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] spec
+main = runSpecAndExitProcess [ consoleReporter ] spec
