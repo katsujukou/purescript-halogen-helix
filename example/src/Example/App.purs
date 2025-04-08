92886248f44938e2ee753f4509eb475a16ef88c8
Katsujukou Kineya <katsujukou@gmail.com>
2025-04-08 23:00:48 +0900

    example

--- example/src/Example/App.purs
+++ example/src/Example/App.purs
@@ -2,15 +2,19 @@ module Example.App where
 
 import Prelude
 
+import Data.Array (fold)
 import Data.Tuple.Nested ((/\))
 import Effect.Class (class MonadEffect)
-import Example.Store (Action(..), useTodos)
+import Example.ComponentA as ComponentA
+import Example.ComponentB as ComponentB
+import Example.Store (Action(..), _todos)
 import Halogen (ClassName(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties (ButtonType(..), InputType(..))
 import Halogen.HTML.Properties as HP
+import Halogen.Helix (useStore)
 import Halogen.Hooks (useState)
 import Halogen.Hooks as Hooks
 import Type.Proxy (Proxy(..))
@@ -33,7 +37,7 @@ app = Hooks.component \_ _ -> Hooks.do
 
 todoItemList :: forall q i o m. MonadEffect m => H.Component q i o m
 todoItemList = Hooks.component \_ _ -> Hooks.do
-  todos /\ ctx <- useTodos identity
+  { items: todos } /\ ctx <- useStore _todos 
 
   let
     classString done = "todo-item " <> case done of
@@ -41,18 +45,21 @@ todoItemList = Hooks.component \_ _ -> Hooks.do
       false -> ""
 
   Hooks.pure do
-    HH.div [] $
-      todos <#> \todo -> do
+    HH.div [] $ fold
+      [ [ HH.slot_ (Proxy :: _ "component-A") unit ComponentA.make {} 
+        , HH.slot_ (Proxy :: _ "component-B") unit ComponentB.make {} 
+        ]
+      , todos <#> \todo -> do
         HH.div
           [ HP.class_ $ ClassName $ classString todo.done
           , HE.onClick \_ -> ctx.dispatch (MarkDone todo.id)
           ]
           [ HH.text todo.title ]
-
+      ]
 addTodoItemForm :: forall q i o m. MonadEffect m => H.Component q i o m
 addTodoItemForm = Hooks.component \_ _ -> Hooks.do
   text /\ textId <- useState ""
-  _ /\ ctx <- useTodos identity
+  _ /\ ctx <- useStore _todos 
 
   let
     handleClick = do
